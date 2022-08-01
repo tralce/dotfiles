334950e8f081e008e2a6166cec7698e22c4c149d
tralce <dan@tralce.com>
2022-08-01 17:54:05 -0400

    f2a59b02-6eb1-435a-b67c-7fab54203dc6

--- .bin/autostart-global.sh
+++ .bin/autostart-global.sh
@@ -16,7 +16,6 @@ then
   sleep 1
 fi
 
-# (while true;do setxkbmap -option compose:menu;setxkbmap -option compose:ralt;sleep 15;done) &
 xset dpms force on
 xset -dpms
 xset s noblank
@@ -28,7 +27,6 @@ run conky -q
 
 run blueman-applet
 run cbatticon
-# run clipit
 run flameshot
 run nm-applet
 run numlockx
