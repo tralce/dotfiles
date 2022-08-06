5bda7705631031318a3c94986eca3243770f64d4
tralce <dan@tralce.com>
2022-08-06 16:04:54 -0400

    bb46a8af-6292-4bad-9c3e-ad65f0e97e81

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -198,7 +198,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 26 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
